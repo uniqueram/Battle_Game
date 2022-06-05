3a8fbdc51c0f47ba98696cd8f399311c2489f729
uniqueram <uniqueram4@gmail.com>
2022-06-05 14:32:04 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from secrets import choice
+from tkinter import BEVEL
+from classes.game import Person, bcolors
+
+
+magic =[{"name": "Fire", "cost": 10, "dmg": 60},
+        {"name": "Thunder", "cost": 10, "dmg": 80},
+        {"name": "Blizzard", "cost": 10, "dmg": 60}]
+
+
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolors.FAIL +bcolors.BOLD + "An enemy Attacks!" + bcolors.ENDC)
+
+while running:
+        print("=======================")
+        player.choose_action()
+        choice = input("Choose Action:")
+        index = int(choice) - 1
+
+        if index == 0:
+                dmg = player.generate_damage()
+                enemy.take_damage(dmg)
+                print("you attacked for", dmg, "points of damage. Enemy hp is:", enemy.get_hp())
+   
+
+
